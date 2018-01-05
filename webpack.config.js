dbb894770b153d032f21f5b42499cb96522fd35e
Innokenty Polyakov <innoken_p@mail.ru>
2018-01-05 15:10:24 +0300

    05.01.2018 15:10

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './public'),
-    publicPath: '/public/',
+    publicPath: '/character-classes-skyrim/public/',
     filename: 'build.js',
     chunkFilename: 'data/[name].js' // для динамической загрузки с помощью import()
   },
